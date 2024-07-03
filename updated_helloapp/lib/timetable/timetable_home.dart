b8d26fc8c1e8d95f45f021b288faacde93d58c41
joncool99 <joncool99@hotmail.com>
2024-07-04 01:46:49 +0800

    Timetable
    Timetable fixed to display lessons

--- updated_helloapp/lib/timetable/timetable_home.dart
+++ updated_helloapp/lib/timetable/timetable_home.dart
@@ -58,7 +58,7 @@ class TimetableHome extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => ViewTimetableScreen()),
+                          builder: (context) => ViewTimetable()),
                     );
                   },
                   style: ElevatedButton.styleFrom(
